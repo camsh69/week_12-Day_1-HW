f4ee819334b970cdecefc33037d0ed434156be59
camsh69 <camsh69@gmail.com>
2022-02-14 19:40:14 +0000

    updated Dodgem class and tests - MVP complete

--- ThemeParkStartPoint/src/test/java/attractions/DodgemTest.java
+++ ThemeParkStartPoint/src/test/java/attractions/DodgemTest.java
@@ -2,12 +2,14 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor visitor;
 
     @Before
     public void setUp() throws Exception {
@@ -28,5 +30,22 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void hasDefaultPrice() {
+        assertEquals(4.50, dodgems.defaultPrice(), 0.0);
+    }
+
+    @Test
+    public void ageUnder12PayHalfPrice() {
+        visitor = new Visitor(11, 1.25, 5.00);
+        assertEquals(2.25, dodgems.priceFor(visitor), 0.0);
+    }
+
+    @Test
+    public void age12UpwardsPayDefault() {
+        visitor = new Visitor(12, 1.35, 10.00);
+        assertEquals(4.50, dodgems.priceFor(visitor), 0.0);
     }
 }
