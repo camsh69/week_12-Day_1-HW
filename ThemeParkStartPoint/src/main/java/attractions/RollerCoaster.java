494c2022e01ef036086a04626fa538957c92f694
camsh69 <camsh69@gmail.com>
2022-02-14 19:15:32 +0000

    updated RollerCoster to implement ITicketed

--- ThemeParkStartPoint/src/main/java/attractions/RollerCoaster.java
+++ ThemeParkStartPoint/src/main/java/attractions/RollerCoaster.java
@@ -2,9 +2,10 @@ package attractions;
 
 import behaviours.IReviewed;
 import behaviours.ISecurity;
+import behaviours.ITicketed;
 import people.Visitor;
 
-public class RollerCoaster  extends Attraction implements ISecurity, IReviewed {
+public class RollerCoaster  extends Attraction implements ISecurity, IReviewed, ITicketed {
 
     public RollerCoaster(String name, int rating) {
         super(name, rating);
@@ -13,5 +14,18 @@ public class RollerCoaster  extends Attraction implements ISecurity, IReviewed {
     @Override
     public boolean isAllowed(Visitor visitor) {
         return visitor.getHeight() >= 145.00;
+    }
+
+    @Override
+    public double defaultPrice() {
+        return 8.40;
+    }
+
+    @Override
+    public double priceFor(Visitor visitor) {
+        if (visitor.getHeight() > 200.00) {
+            return defaultPrice() * 2;
+        }
+        return defaultPrice();
     }
 }
