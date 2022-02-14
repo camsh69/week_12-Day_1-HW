5b639f34b954ced59f1f9ae806bd5ae9383441f5
camsh69 <camsh69@gmail.com>
2022-02-14 18:55:17 +0000

    updated RollerCoaster tests

--- ThemeParkStartPoint/src/test/java/attractions/RollercoasterTest.java
+++ ThemeParkStartPoint/src/test/java/attractions/RollercoasterTest.java
@@ -2,12 +2,14 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class RollercoasterTest {
 
     RollerCoaster rollerCoaster;
+    Visitor visitor;
 
     @Before
     public void setUp() {
@@ -27,5 +29,17 @@ public class RollercoasterTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void isAllowedHeight__True() {
+        visitor = new Visitor(19, 145.0, 25.00);
+        assertTrue(rollerCoaster.isAllowed(visitor));
+    }
+
+    @Test
+    public void isAllowedHeight__False() {
+        visitor = new Visitor(19, 144.9, 25.00);
+        assertFalse(rollerCoaster.isAllowed(visitor));
     }
 }
